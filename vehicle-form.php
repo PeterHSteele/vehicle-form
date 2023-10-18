5a99d4ac5248cb604e472829b8d3bb3b662f7d6e
Peter Steele <steele.peter.3@gmail.com>
2023-10-18 19:42:16 -0400

    add function to output confirmation page

--- vehicle-form.php
+++ vehicle-form.php
@@ -3,7 +3,7 @@
  
  * @wordpress-plugin
  * Plugin Name:       Vehicle form
- * Description:       Support for gravity form allowing users to rent vehicle.
+ * Description:       Support for gravity form allowing users to get a vehicle permit.
  * Version:           1.0.0
  * Author:            Peter Steele
  * Text Domain:       vehicle-form
@@ -187,25 +187,25 @@ define( 'VEHICLE_FORM_COUNTRIES', [
 ]);
 
 define( 'VEHICLE_FORM_PLANS', [
-  [
+  'ten-day' => [
     'name' => __( '10 Day Vignette Austria', 'vehicle-form' ),
     'price' => 12.50,
     'info' => __( 'Valid immediately or later if desired.', 'vehicle-form' ),
     'duration' => 'ten-day'
   ],
-  [
+  'two-month' => [
     'name' => __( '2 Months Vignette Austria', 'vehicle-form'),
     'price' => 23.60,
     'info' => __( 'Valid immediately or later if desired.', 'vehicle-form' ),
     'duration' => 'two-month'
   ],
-  [
+  'annual' => [
     'name' => __( 'Annual Vignette Austria 2024', 'vehicle-form' ),
     'price' => 49.90,
     'info' => __( 'Valid from December 1st, 2023 - January 31st, 2025' ),
     'duration' => 'annual'
   ],
-  [
+  'single' => [
     'name' => __( 'Route Toll', 'vehicle-form' ),
     'price' => 11.50,
     'info' => 'Valid for 1 year from the date of purchase/start of validity',
@@ -220,41 +220,110 @@ require_once plugin_dir_path( __FILE__ ) . 'inputs/cart-terms.php';
 
 function vehicle_form_enqueue( $form, $is_ajax ){
 
-  if ( 3 == $form['id'] || 'sigtest' == $form['title'] ){//change
-    wp_enqueue_style( 'vehicle-form-general' , plugins_url( 'assets/css/general.css', __FILE__ ));
-    wp_enqueue_style( 'montserrat', 'https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap' );
-  }
-  
-  $vehicle_fields = GFAPI::get_fields_by_type( $form, 'vehicle' );
-  if ( count( $vehicle_fields )){
-    wp_enqueue_style( 'vehicle-form-vehicle', plugins_url( '/assets/css/vehicle.css', __FILE__ ), array( 'vehicle-form-general' ));
-    wp_enqueue_script( 'vehicle-form-vehicle', plugins_url( '/assets/js/vehicle.js', __FILE__ ), array( 'jquery' ));
-  }
-
-  $plans_fields = GFAPI::get_fields_by_type( $form, 'pricing-plan' );
-  if ( count( $plans_fields ) ){
-    wp_enqueue_style( 'vehicle-form-plan', plugins_url( '/assets/css/plans.css', __FILE__ ), array( 'vehicle-form-general' ));
-    wp_enqueue_script( 'vehicle-form-plan', plugins_url( 'assets/js/plans.js', __FILE__ ));
-    wp_localize_script( 'vehicle-forms-cart-terms', 'vfplans', VEHICLE_FORM_PLANS );
-  }
-
   $license_plate_fields = GFAPI::get_fields_by_type( $form, 'license_plate' );
   if ( count( $license_plate_fields )){
-    wp_enqueue_style( 'vehicle-form-license', plugins_url( 'assets/css/license-plate.css', __FILE__ ), array( 'vehicle-form-general' ));
-    wp_enqueue_script( 'vehicle-form-license', plugins_url( 'assets/js/license-plate.js', __FILE__ ));
-    wp_enqueue_style( 'gforms_datepicker_css', plugins_url( '/gravityforms/legacy/css/datepicker.min.css'));
-    wp_enqueue_script( 'vehicle-form-date' , plugins_url( 'assets/js/date.js', __FILE__ ), array( 'jquery', 'jquery-ui-datepicker' ));
-
-    wp_localize_script( 'vehicle-form-license', 'countries', VEHICLE_FORM_COUNTRIES );
-  }
-
-  $cart_terms_fields = GFAPI::get_fields_by_type( $form, 'cart_and_terms' );
-  if ( count( $cart_terms_fields )){
-    wp_enqueue_style( 'vehicle-forms-cart-terms', plugins_url( 'assets/css/cart-terms.css', __FILE__), array( 'vehicle-form-general' ));
-    wp_enqueue_script( 'vehicle-forms-cart-terms', plugins_url( 'assets/js/cart.js', __FILE__ ));
-    wp_localize_script( 'vehicle-forms-cart-terms', 'vfplans', VEHICLE_FORM_PLANS );
-    wp_localize_script( 'vehicle-forms-cart-terms', 'img', array( 'src' => VEHICLE_FORM_PLATES . 'close.svg' ));
+    wp_enqueue_style( 'montserrat', 'https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap' );
+    wp_enqueue_style( 'vehicle-form-all', plugins_url(  'assets/css/all.min.css', __FILE__ ));
+    wp_enqueue_script( 'vehicle-form-all', plugins_url(  'assets/js/all.min.js', __FILE__  ), array( 'jquery', 'jquery-ui-datepicker' ));
+    wp_localize_script( 'vehicle-form-all', 'countries', VEHICLE_FORM_COUNTRIES );
+    wp_localize_script( 'vehicle-form-all', 'vfplans', VEHICLE_FORM_PLANS );
+    wp_localize_script( 'vehicle-form-all', 'img', array( 'src' => VEHICLE_FORM_PLATES . 'close.svg' ));
   }
 }
 
 add_action( 'gform_enqueue_scripts', 'vehicle_form_enqueue', 10, 2 );
+
+function vehicle_form_enqueue_confirmation_style(){
+  wp_enqueue_style( 'vehicle-form-confirmation', plugins_url( 'assets/css/confirmation.css', __FILE__ ));
+}
+
+add_action( 'wp_enqueue_scripts', 'vehicle_form_enqueue_confirmation_style' );
+
+function vehicle_form_register_query_vars( $vars ){
+  $vehicle_form_vars = array( 'vf-plan', 'vf-firstname', 'vf-lastname', 'vf-invoice', 'vf-company', 'vf-street', 'vf-region', 'vf-city', 'vf-zip' );
+  $vars = array_merge( $vars, $vehicle_form_vars );
+  return $vars;
+}
+
+add_filter( 'query_vars', 'vehicle_form_register_query_vars' );
+
+function vehicle_form_filter_content( $content ){
+  global $post;
+  if ( 198 != $post->ID ){//change
+    return $content;
+  } else {
+
+    $plan_param = get_query_var( 'vf-plan', '' );
+    if ( '' == $plan_param || !isset( VEHICLE_FORM_PLANS[ $plan_param ] ) ){
+      $plan = __( 'Your order', 'vehicle-form' ); 
+    } else {
+      $plan = esc_html( VEHICLE_FORM_PLANS[ $plan_param ]['name'] );
+    }
+
+    $invoice_var = get_query_var( 'vf-invoice', '' );
+    $needs_invoice = $invoice_var == "I need an invoice";
+    
+    $firstname = ucfirst( get_query_var( 'vf-firstname', '' ));
+    $h2_string =  empty( $firstname ) ? __('Thank you!', 'vehicle-form') : __( 'Thank you, %1$s!', 'vehicle-form' );
+    $h2 = sprintf( $h2_string, esc_html( $firstname ) );
+    $intro = esc_html__( 'For your records, please find your order information listed below.', 'vehicle-form' );
+    $price = isset( $_GET[$plan_param] ) ? $_GET[$plan_param] : '';
+    $price_string = !empty( $price ) ? sprintf( '<p><strong>Total:</strong> %1$s €</p>', esc_html( $price ) ) : '';
+
+    $invoice_info = "";
+    if ( $needs_invoice ){
+      $issued_to = sprintf( 
+        __( '<strong>Issued to</strong>: %1$s' ), 
+        esc_html( $firstname . ' ' . get_query_var( 'vf-lastname', '' )),
+      );
+
+      $company = get_query_var( 'vf-company', '' );
+  
+      $company_label = sprintf( '<strong>%1$s:</strong> ', __( 'Company', 'vehice-form' ) );
+      $company_text = $company_label . esc_html( ucfirst( $company ));
+      
+      $street = get_query_var( 'vf-street', '' );
+      $city = esc_html( get_query_var( 'vf-city', '' ));
+      $region = esc_html( get_query_var( 'vf-region', '' ) );
+      $street_address = sprintf( 
+        '<strong>%1$s:</strong> %2$s', 
+        __( 'Street Address', 'vehicle-form' ),
+        esc_html( $street ),
+      );
+      $postal_code = esc_html( get_query_var( 'vf-zip', '' ));
+
+      $invoice_info = "
+        <div class='invoice-info'>
+          <div class='issued-to'>{$issued_to}</div>
+          <div class='company'>{$company_text}</div>
+          <div class='vf-address'>{$street_address}</div>
+          <div class='vf-city'><strong>City:</strong> {$city}</div>
+          <div class='vf-region'><strong>Region:</strong> {$region}</div>
+          <div class='vf-zip'><strong>Postal Code</strong> {$postal_code}</div>
+        </div>
+      ";
+    }
+
+
+    $content = "
+      <article class='vf-confirmation'>
+        <header>
+          <h2>{$h2}</h2>
+        </header>
+        <div class='vf-conf-content'>
+          <p>{$intro}</p>
+          <div class='order-summary-wrap'>
+            <div class='order-summary'>
+              <h3>{$plan}</h3>
+              {$price_string}
+              {$invoice_info}
+            </div>
+          </div>
+        </div>
+      </article>
+    ";
+    return $content;
+  }
+}
+
+add_filter( 'the_content', 'vehicle_form_filter_content' );
